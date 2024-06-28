9062153f3f1714cad69b291f203ad631fc815703
worldonlinegame <motar3000@hotmail.com>
2024-06-28 23:34:39 +0200

    sdassd

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import {firestore} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 const db = firebase.firestore();
+
+
+//
 
 const taskForm = document.getElementById('task-form');
 
 taskForm.addEventListener('submit', async (e) =>{
     e.preventDefault();
+    
    const title = taskForm['task-title'].value;
    const descripcion = taskForm['task-descripcion'].value;
    const response = await db.collection('tasks').doc().set({
