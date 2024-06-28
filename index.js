d603278db88c5ffbe15a6ddb0b22559d93171ea2
worldonlinegame <motar3000@hotmail.com>
2024-06-28 22:51:47 +0200

    sigo maquetando

---
+++ index.js
@@ -0,0 +1,8 @@
+const taskForm = document.getElementById('task-form');
+
+taskForm.addEventListener('submit', e =>{
+    e.preventDefault();
+   const title = taskForm['task-title'].value;
+   const descripcion = taskForm['task-descripcion'].value;
+    console.log(title, descripcion);
+})
