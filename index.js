64788b4e2e0dea685d5987534ed2e260e323a117
worldonlinegame <motar3000@hotmail.com>
2024-06-29 00:25:46 +0200

    tgdfdg

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-import { db } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-const db = firebase.firestore();
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
+const storage = getStorage();
+const storageRef = ref(storage, 'images/rivers.jpg');
 
-//
+const uploadTask = uploadBytesResumable(storageRef, file);
 
-const taskForm = document.getElementById('task-form');
-
-taskForm.addEventListener('submit', async (e) =>{
-    e.preventDefault();
-    
-   const title = taskForm['task-title'].value;
-   const descripcion = taskForm['task-descripcion'].value;
-   const response = await db.collection('tasks').doc().set({
-        title,
-        descripcion
-    })
-
-    console.log(response);
-})
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+);
