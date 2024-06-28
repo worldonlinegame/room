4134c9d1f51f38dfffb749368558e3a946d2eee6
worldonlinegame <motar3000@hotmail.com>
2024-06-28 23:21:35 +0200

    dfas

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-firebase.firestore();
+const db = firebase.firestore();
 
 const taskForm = document.getElementById('task-form');
 
-taskForm.addEventListener('submit', e =>{
+taskForm.addEventListener('submit', async (e) =>{
     e.preventDefault();
    const title = taskForm['task-title'].value;
    const descripcion = taskForm['task-descripcion'].value;
-    console.log(title, descripcion);
+   const response = await db.collection('tasks').doc().set({
+        title,
+        descripcion
+    })
+
+    console.log(response);
 })
